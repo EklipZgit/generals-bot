f25be1283b158e1b4b32aa71734c532c107fec3c
enedil <enedil@anton.tail5e8d.ts.net>
2025-01-24 04:07:52 +0100

    cpp: kinda working knapsack

--- cpp/KnapsackUtilsPy.py
+++ cpp/KnapsackUtilsPy.py
@@ -1,57 +1,11 @@
-// cppimport
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <vector>
+import cppimport.import_hook
+import logbook
+import math
+import time
+import typing
 
-using IntList = std::vector<int>;
+from KnapsackUtilsCpp import multiple_choice_knapsack_loop
 
-std::vector<std::vector<int>>& multiple_choice_knapsack_loop(
-    size_t capacity,
-    size_t n,
-    IntList& weights,
-    IntList& values,
-    IntList& groups,
-    std::vector<std::vector<int>>& K,
-    std::vector<std::pair<int, int>>& groupStartEnds
-) {
-    for (int curCapacity = 0; curCapacity < capacity + 1; ++curCapacity) {
-        for (size_t i = 0; i < n+1; ++i) {
-            if (i == 0 || curCapacity == 0) {
-                K[i][curCapacity] = 0;
-            } else if (weights[i - 1] <= curCapacity) {
-                int sub_max = 0;
-                int prev_group = groups[i-1] - 1;
-                int subKRow = curCapacity - weights[i - 1];
-                if (prev_group >= 0) {
-                    auto [prevGroupStart, prevGroupEnd] = groupStartEnds[prev_group];
-                    for (int j = prevGroupStart + 1; j < prevGroupEnd + 1; ++j) {
-                        if (groups[j - 1] == prev_group && K[j][subKRow] > sub_max) {
-                            sub_max = K[j][subKRow];
-                        }
-                    }
-                }
-                K[i][curCapacity] = std::max(sub_max + values[i - 1], K[i - 1][curCapacity]);
-            } else {
-                K[i][curCapacity] = K[i - 1][curCapacity];
-            }
-        }
-    }
-    return K;
-}
-
-PYBIND11_MODULE(KnapsackUtilsCpp, m) {
-    m.doc() = "multiple_choice_knapsack_loop native"; // optional module docstring
-
-    m.def("multiple_choice_knapsack_loop", &multiple_choice_knapsack_loop, "multiple_choice_knapsack_loop native");
-}
-/*
-<%
-setup_pybind11(cfg)
-%>
-*/
-
-
-/*
 def solve_multiple_choice_knapsack(
         items: typing.List[typing.Any],
         capacity: int,
@@ -129,23 +83,8 @@ def solve_multiple_choice_knapsack(
     if not noLog:
         logbook.info(f'estimated knapsack time: {estTime:.3f} (n {n} * capacity {capacity} * math.sqrt(maxGroupSize {maxGroupSize}) {maxGrSq:.1f})')
 
-    for curCapacity in range(capacity + 1):
-        for i in range(n + 1):
-            if i == 0 or curCapacity == 0:
-                K[i][curCapacity] = 0
-            elif weights[i - 1] <= curCapacity:
-                sub_max = 0
-                prev_group = groups[i - 1] - 1
-                subKRow = curCapacity - weights[i - 1]
-                if prev_group > -1:
-                    prevGroupStart, prevGroupEnd = groupStartEnds[prev_group]
-                    for j in range(prevGroupStart + 1, prevGroupEnd + 1):
-                        if groups[j - 1] == prev_group and K[j][subKRow] > sub_max:
-                            sub_max = K[j][subKRow]
-                K[i][curCapacity] = max(sub_max + values[i - 1], K[i - 1][curCapacity])
-            else:
-                K[i][curCapacity] = K[i - 1][curCapacity]
-
+    K = multiple_choice_knapsack_loop(capacity,n,weights,values,groups,K, groupStartEnds)
+    
     res = K[n][capacity]
     timeTaken = time.perf_counter() - timeStart
     if not noLog:
@@ -195,83 +134,3 @@ def solve_multiple_choice_knapsack(
             f"multiple choice knapsack completed on {n} items for capacity {capacity} finding value {K[n][capacity]} in Duration {time.perf_counter() - timeStart:.3f}")
 
     return K[n][capacity], includedItems
-
-
-def solve_knapsack(
-        items: typing.List[typing.Any],
-        capacity: int,
-        weights: typing.List[int],
-        values: typing.List[int]
-) -> typing.Tuple[int, typing.List[typing.Any]]:
-    """
-    Stolen from https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
-    Python3 code for Dynamic Programming
-    based solution for 0-1 Knapsack problem
-
-    returns the combined value, and list of items that fit in a knapsack of a given capacity.
-    @param items:
-    @param capacity:
-    @param weights:
-    @param values:
-    @return:
-    """
-    for value in values:
-        if not isinstance(value, int):
-            raise AssertionError('values are all required to be ints or this algo will not function')
-
-    timeStart = time.perf_counter()
-    n = len(items)
-    K = [[0 for w in range(capacity + 1)]
-         for i in range(n + 1)]
-
-    # Build table K[][] in bottom up manner
-    for i in range(n + 1):
-        for w in range(capacity + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif weights[i - 1] <= w:
-                K[i][w] = max(
-                    values[i - 1] + K[i - 1][w - weights[i - 1]],
-                    K[i - 1][w])
-            else:
-                K[i][w] = K[i - 1][w]
-
-    # stores the result of Knapsack
-    res = K[n][capacity]
-    logbook.info("Value Found {}".format(res))
-    includedItems = []
-    w = capacity
-    for i in range(n, 0, -1):
-        if res <= 0:
-            break
-        if i == 0:
-            raise AssertionError("i == 0 in knapsack items determiner?? res {} i {} w {}".format(res, i, w))
-            break
-        if w < 0:
-            raise AssertionError("w < 0 in knapsack items determiner?? res {} i {} w {}".format(res, i, w))
-            break
-        # either the result comes from the
-        # top (K[i-1][w]) or from (val[i-1]
-        # + K[i-1] [w-wt[i-1]]) as in Knapsack
-        # table. If it comes from the latter
-        # one/ it means the item is included.
-        # THIS IS WHY VALUE MUST BE INTS
-        if res == K[i - 1][w]:
-            continue
-
-        # This item is included.
-        logbook.info(
-            f"item at index {i - 1} with value {values[i - 1]} and weight {weights[i - 1]} was included... adding it to output. (Res {res})")
-        includedItems.append(items[i - 1])
-
-        # Since this weight is included
-        # its value is deducted
-        res = res - values[i - 1]
-        w = w - weights[i - 1]
-
-    logbook.info(
-        f"knapsack completed on {n} items for capacity {capacity} finding value {K[n][capacity]} in Duration {time.perf_counter() - timeStart:.3f}")
-    return K[n][capacity], includedItems
-
-
-*/

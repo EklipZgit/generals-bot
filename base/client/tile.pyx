75a6e7edb668ccc4dc2984086cec8c0da993ea78
enedil <enedil@anton.tail5e8d.ts.net>
2025-01-28 03:30:12 +0100

    bleh

--- base/client/tile.pyx
+++ base/client/tile.pyx
@@ -464,41 +464,44 @@ class Tile(object):
 
     @staticmethod
     def convert_player_to_char(player: int):
-        match player:
-            case -1:
-                return 'N'
-            case 0:
-                return 'a'
-            case 1:
-                return 'b'
-            case 2:
-                return 'c'
-            case 3:
-                return 'd'
-            case 4:
-                return 'e'
-            case 5:
-                return 'f'
-            case 6:
-                return 'g'
-            case 7:
-                return 'h'
-            case 8:
-                return 'i'
-            case 9:
-                return 'j'
-            case 10:
-                return 'k'
-            case 11:
-                return 'l'
-            case 12:
-                return 'm'
-            case 13:
-                return 'n'
-            case 14:
-                return 'o'
-            case 15:
-                return 'p'
+        if player == -1:
+            return 'N'
+        return 'abcdefghijklmop'[player]
+        # match player:
+        #     case -1:
+        #         return 'N'
+        #     case 0:
+        #         return 'a'
+        #     case 1:
+        #         return 'b'
+        #     case 2:
+        #         return 'c'
+        #     case 3:
+        #         return 'd'
+        #     case 4:
+        #         return 'e'
+        #     case 5:
+        #         return 'f'
+        #     case 6:
+        #         return 'g'
+        #     case 7:
+        #         return 'h'
+        #     case 8:
+        #         return 'i'
+        #     case 9:
+        #         return 'j'
+        #     case 10:
+        #         return 'k'
+        #     case 11:
+        #         return 'l'
+        #     case 12:
+        #         return 'm'
+        #     case 13:
+        #         return 'n'
+        #     case 14:
+        #         return 'o'
+        #     case 15:
+        #         return 'p'
 
     def __str__(self) -> str:
         return f"{self.x},{self.y}"

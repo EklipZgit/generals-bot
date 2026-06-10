416cc065f9448443c850b721b0e3951344ba2015
Travis Drake <tdrake0x45@gmail.com>
2026-06-10 14:43:12 -0700

    Fix for moving our own general around all fuckeduppedly

--- Tests/test_GeneralPrediction.py
+++ Tests/test_GeneralPrediction.py
@@ -1568,3 +1568,22 @@ class GeneralPredictionTests(TestBase):
         self.assertValidGeneralPositionXY(bot, 12, 17, enemyGeneral.player)
         self.assertInvalidGeneralPositionXY(bot, 16, 16, enemyGeneral.player)
 
+    def test_should_not_move_our_own_general_around_the_board_randomly(self):
+        debugMode = not TestBase.GLOBAL_BYPASS_REAL_TIME_TEST and True
+        mapFile = 'GameContinuationEntries/should_not_move_our_own_general_around_the_board_randomly___UVN7hAajg---3--208.txtmap'
+        map, general, enemyGeneral = self.load_map_and_generals(mapFile, 208, fill_out_tiles=True)
+        self.move_enemy_city(map, 4,13, 0,17)
+
+        rawMap, _ = self.load_map_and_general(mapFile, respect_undiscovered=True, turn=208)
+
+        self.enable_search_time_limits_and_disable_debug_asserts()
+        simHost = GameSimulatorHost(map, player_with_viewer=general.player, playerMapVision=rawMap, allAfkExceptMapPlayer=True)
+        simHost.queue_player_moves_str(general.player, '5,11->5,12')
+        bot = self.get_debug_render_bot(simHost, general.player)
+        playerMap = simHost.get_player_map(general.player)
+
+        self.begin_capturing_logging()
+        winner = simHost.run_sim(run_real_time=debugMode and not self.GLOBAL_BYPASS_RENDERING, turn_time=0.25, turns=1)
+        self.assertNoFriendliesKilled(map, general)
+
+        # Runtime would throw if bug weren't fixed
